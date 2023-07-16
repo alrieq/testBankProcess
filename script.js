a63b5d17b1b05ffccf036e37f8fca62a33c927e1
alrieq <alrieq@live.com>
2023-07-16 15:41:45 +0300

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+let login = document.getElementById("logIn");
+let id = document.getElementById("id").innerText;
+let instructorId = 2035930;
+let adminId = 2035931;
+
+
+// if(id === adminId){
+//     login.setAttribute("href", "admin.html");
+// }else if(id === instructorId){
+//     login.setAttribute("href", "instructor.html");
+// }
